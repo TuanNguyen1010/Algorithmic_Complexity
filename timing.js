c5ad6127a80f14c0428714385e9cfa2e77390ed5
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-09 12:19:52 +0000

    timing functionailty added

--- timing.js
+++ timing.js
@@ -1,16 +1,34 @@
 const {performance} = require('perf_hooks')
  
+
+function createRandomArray(arraySize) {
+  array = []
+  var i;
+  for (i = 0; i< arraySize; i++) {
+  array.push(Math.round(Math.random() * 100))
+  }
+  return array
+}
+
 function shuffle(sampleArray) {
   [sampleArray].shuffle
 
 }
 
+function sort(sampleArray){
+  (sampleArray).sort
+}
+
 function timingCode() {
+  var i;
+  for (i = 5000; i < 100000; i+= 5000) {
+    testingArray = createRandomArray(i)
     var t0 = performance.now();
-    shuffle()
+    sort(testingArray)
     var t1 = performance.now();
 
-    console.log("doSomething took " + (t1 - t0) + " millisecond")
+    console.log("sort for " + i + " took ; " + (t1 - t0) + " millisecond")}
 }
 
 timingCode()
+// console.log(createRandomArray(10000));
